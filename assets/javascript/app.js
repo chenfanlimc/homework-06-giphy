04ec762e43e86e559163036a1e01409aee02db51
cli <cli@mastercontrol.com>
2018-05-21 17:58:53 -0600

    enhanced functionality a bit

--- assets/javascript/app.js
+++ assets/javascript/app.js
@@ -10,7 +10,7 @@ $(document).ready(function () {
         $(".topic-buttons").empty();
         for (var i = 0; i < topics.length; i++) {
             var newButton = $("<button>");
-            newButton.addClass("topic-button");
+            newButton.addClass("btn btn-info");
             newButton.attr("data-value", topics[i]);
             newButton.text(topics[i]);
             $(".topic-buttons").append(newButton);
@@ -31,6 +31,15 @@ $(document).ready(function () {
         }
     })
 
+    $(".clearGifs").on("click", function() {
+        $(".gifs").empty();
+    })
+
+    $(".resetTopic").on("click", function() {
+        topics = ["final fantasy", "jumanji", "john wick", "friday", "sunshine", "cat", "disney"];
+        createTopic();
+    })
+
     //adds functionality to buttons, making ajax call to get the appropriate gifs
     function buttonRender() {
         $("button").on("click", function () {
@@ -46,17 +55,36 @@ $(document).ready(function () {
                 var objectData = response.data;
                 for (var i = 0; i < objectData.length; i++) {
                     console.log(objectData)
+                    var card = $("<div>");
+                    card.addClass("imageContainer");
+                    card.attr("style", "width: 100%");
+                    var cardBody = $("<div>");
+                    cardBody.addClass("card-body");
+                    card.prepend(cardBody);
+                    var source = $("<div>");
+                    source.addClass("card-text");
+                    source.html("<b>Source:</b> " + objectData[i].source);
+                    cardBody.prepend(source);
                     var rating = $("<div>");
-                    rating.addClass("col-md-12");
-                    rating.text("Rating: " + (objectData[i].rating).toUpperCase());
-                    $(".gifs").prepend(rating);
+                    rating.addClass("card-text");
+                    rating.html("<b>Rating:</b> " + (objectData[i].rating).toUpperCase());
+                    cardBody.prepend(rating);
+                    var title = $("<div>");
+                    title.addClass("card-text");
+                    title.html("<b>Title:</b> " + objectData[i].title);
+                    cardBody.prepend(title);
                     var newGif = $("<img>");
                     newGif.addClass("gif");
-                    newGif.attr("src", objectData[i].images.downsized_still.url);
-                    newGif.attr("data-still", objectData[i].images.downsized_still.url);
-                    newGif.attr("data-animate", objectData[i].images.downsized_medium.url);
+                    newGif.attr("src", objectData[i].images.fixed_height_still.url);
+                    newGif.attr("data-still", objectData[i].images.fixed_height_still.url);
+                    newGif.attr("data-animate", objectData[i].images.fixed_height.url);
                     newGif.attr("data-state", "still");
-                    $(".gifs").prepend(newGif);
+                    newGif.attr("style", "width: 50%");
+                    card.prepend(newGif);
+                    var download = $('<a download></a>').attr("href", newGif.attr("data-animate"));
+                    download.text("Download");
+                    cardBody.prepend(download);
+                    $(".gifs").prepend(card);
                 }
 
                 //animate vs stop gifs
